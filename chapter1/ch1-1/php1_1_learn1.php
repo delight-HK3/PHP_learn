4d059fe4c25cde58fa7bdcf9dbec230f5470f8c9
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-03 09:02:50 +0900

    Add files via upload

---
+++ chapter1/ch1-1/php1_1_learn1.php
@@ -0,0 +1,13 @@
+<html>
+    	<head>
+    	</head>
+    	<body>
+        	<?php
+            	# php 주석 테스트 입니다.
+            	// php 주석 테스트 입니다.
+           		/*
+                		php 주석 테스트 입니다.
+            	*/
+        	?>
+	</body>
+</html>
