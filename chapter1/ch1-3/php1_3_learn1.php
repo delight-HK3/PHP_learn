b78bf1962fe5299438d150651c9f0c01e3b67990
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-05 08:59:50 +0900

    Add files via upload
    ch1-3을 업로드 했습니다.

---
+++ chapter1/ch1-3/php1_3_learn1.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+			$str1 = "hello"; 
+			//php에서는 큰따옴표 작은따옴표 관계없이 문자열 작성이 가능합니다.
+			$str2 = 'hello';
+			
+			echo $str1." ".$str2;
+		?>
+	</body>
+</html>
---
+++ chapter1/ch1-3/php1_3_learn3.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+			$flo1 = 3.14;
+			$flo2 = -3.14;
+			var_dump($flo1);
+			echo "<br>";
+			var_dump($flo2);
+		?>
+	</body>
+</html>
