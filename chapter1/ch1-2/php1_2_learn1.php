1044ecf80383b630c275b6fca240d82abf381803
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-04 10:53:58 +0900

    Add files via upload
    ch1-2를 추가했습니다.

---
+++ chapter1/ch1-2/php1_2_learn1.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+            echo "echo test<br>";
+            ecHo "ecHo test<br>";
+            Echo "Echo test<br><br>";
+
+            print "print test<br>";
+            Print "Print test<br>";
+            prinT "prinT test<br>";
+        ?>
+    </body>
+</html>
---
+++ chapter1/ch1-2/php1_2_learn2.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+            $txt = "Hello";
+            $intx = 10;
+            $floaty = 3.14;
+
+            echo $txt."<br>"; // Hello
+            echo $intx."<br>"; // 10
+            echo $floaty."<br>"; // 3.14
+        ?>
+    </body>
+</html>
