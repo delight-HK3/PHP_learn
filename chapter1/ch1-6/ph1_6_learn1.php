929637879e7a98d986bfba183b61876af743b71d
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-12 09:10:41 +0900

    Add files via upload
    ch1-6을 추가했습니다.

---
+++ chapter1/ch1-6/ph1_6_learn1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            //파이함수
+            // 파이값을 반환합니다.
+            echo pi();
+            echo "<br>";
+        ?>
+    </body>
+</html>
---
+++ chapter1/ch1-6/ph1_6_learn2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            echo min(0, 10, -10, 30, -11, 20); // -11
+            echo "<br>";
+            echo max(0, 10, -10, 30, -11, 20); // 30
+            echo "<br>";
+        ?>
+    </body>
+</html>
